71233de2a8e2b0b94e661556a8e49c30d79038e6
Luiz Real <luiz.real@elo7.com>
2023-01-10 11:59:44 -0300

    Configured node adapter; works

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,20 @@ import { fileURLToPath } from 'node:url';
 import { dirname, join } from 'node:path';
 
 // https://astro.build/config
+import node from "@astrojs/node";
+
+// https://astro.build/config
 export default defineConfig({
-	srcDir: './src/main',
-	vite: {
-		resolve: {
-			alias: {
-				'@': join(dirname(fileURLToPath(import.meta.url)), 'src', 'main'),
-			}
-		}
-	}
+  output: 'server',
+  srcDir: './src/main',
+  vite: {
+    resolve: {
+      alias: {
+        '@': join(dirname(fileURLToPath(import.meta.url)), 'src', 'main')
+      }
+    }
+  },
+  adapter: node({
+	mode: 'standalone',
+  })
 });
