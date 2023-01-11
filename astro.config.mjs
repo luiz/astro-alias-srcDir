85814517bcec39f7b0e941d30cfc91100cd89559
Luiz Real <luiz.real@elo7.com>
2023-01-11 17:35:58 -0300

    Removed srcDir configuration, keeping only vite alias; works on dev, bulid breaks

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,11 +11,10 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  srcDir: './src/main',
   vite: {
     resolve: {
       alias: {
-        '@': join(dirname(fileURLToPath(import.meta.url)), 'src', 'main')
+        '@': join(dirname(fileURLToPath(import.meta.url)), 'src')
       }
     }
   },
